b874c01ea29dd0dc1ff9c4d1f723a8f4cdf6c957
Oliver Davies <oliver@oliverdavies.uk>
2020-06-29 21:18:28 +0100

    Add missing return type

--- src/ValueObject/Event.php
+++ src/ValueObject/Event.php
@@ -11,7 +11,7 @@ class Event
 
     private string $link;
 
-    public static function createFromArray(array $data)
+    public static function createFromArray(array $data): self
     {
         return new static($data);
     }
