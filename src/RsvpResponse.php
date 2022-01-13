78841ad9d6ba20c5679f13c37d6965fe9c323ef8
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 20:54:29 +0000

    refactor: add RsvpResponse

---
+++ src/RsvpResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+final class RsvpResponse
+{
+    public const RESPONSE_NO = 'no';
+    public const RESPONSE_YES = 'yes';
+}
--- tests/FakeEventRepository.php
+++ tests/FakeEventRepository.php
@@ -5,20 +5,22 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use App\EventRepository;
+use App\RsvpResponse;
 use Tightenco\Collect\Support\Collection;
 
 final class FakeEventRepository implements EventRepository
 {
     private static array $rsvps = [
-        ['name' => 'matthew s.', 'response' => 'yes'],
-        ['name' => 'Michael P.', 'response' => 'yes'],
-        ['name' => 'Kathryn "Kat" R.', 'response' => 'yes'],
-        ['name' => 'Did not attend', 'response' => 'no'],
+        ['name' => 'matthew s.', 'response' => RsvpResponse::RESPONSE_YES],
+        ['name' => 'Michael P.', 'response' => RsvpResponse::RESPONSE_YES],
+        ['name' => 'Kathryn "Kat" R.', 'response' => RsvpResponse::RESPONSE_YES],
+        ['name' => 'Did not attend', 'response' => RsvpResponse::RESPONSE_NO],
     ];
 
     public function getConfirmedAttendees(): Collection {
         return Collection::make(self::$rsvps)
-            ->filter(fn (array $attendee): bool => $attendee['response'] == 'yes')
+            ->filter(fn (array $attendee): bool => $attendee['response']
+                == RsvpResponse::RESPONSE_YES)
             ;
     }
 }
