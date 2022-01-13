133cc8c8221a2d32682ce1dc2dd65dd3149d6d02
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 20:22:57 +0000

    refactor: use a Collection for attendees

--- tests/EventRepositoryTest.php
+++ tests/EventRepositoryTest.php
@@ -3,17 +3,18 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Tightenco\Collect\Support\Collection;
 
 final class EventRepositoryTest extends KernelTestCase
 {
     /** @test */
     public function should_only_return_attendees_with_a_yes_rsvp(): void {
-        $attendees = [
+        $attendees = Collection::make([
             ['name' => 'Matthew Setter'],
             ['name' => 'Matthew Setter'],
             ['name' => 'Matthew Setter'],
-        ];
+        ]);
 
-        $this->assertCount(3, $attendees);
+        $this->assertCount(3, $attendees->pluck('name'));
     }
 }
