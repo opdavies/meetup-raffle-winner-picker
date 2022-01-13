bc747a1f70e9e1dc35b1035ffdaf754e73bfe3a3
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 21:02:13 +0000

    refactor: get the repository from the container

--- tests/FakeEventRepositoryTest.php
+++ tests/FakeEventRepositoryTest.php
@@ -2,13 +2,16 @@
 
 namespace App\Tests;
 
+use App\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 final class FakeEventRepositoryTest extends KernelTestCase
 {
     /** @test */
     public function should_only_return_attendees_with_a_yes_rsvp(): void {
-        $repository = new FakeEventRepository();
+        $container = self::bootKernel()->getContainer();
+        $repository = $container->get(EventRepository::class);
+
         $attendees = $repository->getConfirmedAttendees();
 
         $this->assertCount(3, $attendees->pluck('name'));
