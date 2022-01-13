6883f63d95abc0d2597d5acd15638d0beff102a8
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 20:18:14 +0000

    test: add the first assertion

--- tests/EventRepositoryTest.php
+++ tests/EventRepositoryTest.php
@@ -7,7 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 final class EventRepositoryTest extends KernelTestCase
 {
     /** @test */
-    public function should_only_return_attendees_with_a_yes_rsvp(): array {
-        return [];
+    public function should_only_return_attendees_with_a_yes_rsvp(): void {
+        $attendees = [
+            ['name' => 'Matthew Setter'],
+            ['name' => 'Matthew Setter'],
+            ['name' => 'Matthew Setter'],
+        ];
+
+        $this->assertCount(3, $attendees);
     }
 }
