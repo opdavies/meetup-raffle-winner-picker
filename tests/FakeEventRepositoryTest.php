f192dd720c9b21777dacccc5ad2116b25274bd8c
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 20:29:32 +0000

    refactor: rename test class

--- tests/FakeEventRepositoryTest.php
+++ tests/FakeEventRepositoryTest.php
@@ -5,7 +5,7 @@ namespace App\Tests;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Tightenco\Collect\Support\Collection;
 
-final class EventRepositoryTest extends KernelTestCase
+final class FakeEventRepositoryTest extends KernelTestCase
 {
     /** @test */
     public function should_only_return_attendees_with_a_yes_rsvp(): void {
